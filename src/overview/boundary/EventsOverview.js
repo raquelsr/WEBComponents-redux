f3ea5e5912f0b44ef274c0c75cbf7b8819e63805
raquelsr <raquelserranor@hotmail.com>
2020-08-04 14:30:22 +0200

    27 - Add filter component

--- src/overview/boundary/EventsOverview.js
+++ src/overview/boundary/EventsOverview.js
@@ -1,11 +1,13 @@
 import { html, render } from '../../lib/lit-html.js';
 import AirElement from '../../AirElement.js';
+import '../../filter/boundary/EventsFilter.js';
 
 class EventsOverview extends AirElement {
 
   view() {
     const eventList = this.state.events;
      return html `
+     <a-events-filter></a-events-filter>
      <ol>
       ${eventList.map(({eventname, description}) => html `
         <li> 
