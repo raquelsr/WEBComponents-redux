7df30162d677a85e798a41ab71ac16b2c469d3f6
raquelsr <raquelserranor@hotmail.com>
2020-08-04 12:34:10 +0200

    07 Inputs and change elements

--- src/creation/boundary/NewEvent.js
+++ src/creation/boundary/NewEvent.js
@@ -1,16 +1,28 @@
 import { render, html } from '../../lib/lit-html.js';
 
 class NewEvent extends HTMLElement {
+
+  constructor() {
+    super();
+    this.event = {};
+  }
+
   connectedCallback() {
     const template = html `
-      <input placeholder="name">
+      <input name="eventname" @change=${e => this.onUserInput(e)} placeholder="name">
+      <input name="description" @change=${e => this.onUserInput(e)} placeholder="description">
       <button @click=${_ => this.newEvent()}>Create</button>
     `;
     render(template, this);
   }
 
   newEvent() {
-    console.log('Button clicked!');
+    console.log('Saving..', this.event);
+  }
+
+  onUserInput({target: {name, value}}) {
+    console.log(name, value);
+    this.event[name] = value;
   }
 }
 
