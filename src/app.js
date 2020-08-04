ad565e9b01c77b144668009c798338c16998cb1c
raquelsr <raquelserranor@hotmail.com>
2020-08-04 11:41:02 +0200

    02 - Redux store creation - example

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import { createStore } from "./lib/redux.js";
+
+const multiply = (state = 21, action) => {
+  console.log(state, action);
+  if (action.type === "M") {
+    return state * action.payload;
+  }
+  return state;
+}
+
+const store = createStore(multiply);
+
+store.subscribe(_ => console.log('state changed', store.getState()));
+
+const initial = store.getState();
+console.log('Initial state', initial);
+
+store.dispatch({
+  type: 'M',
+  payload: 2
+});
+
+console.log(store.getState());
