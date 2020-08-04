9302e5734813170585bdbd0156f1cbdb93bd76b7
raquelsr <raquelserranor@hotmail.com>
2020-08-04 13:49:41 +0200

    15 - Fix copy by reference - not rename data

--- src/creation/control/EventsControl.js
+++ src/creation/control/EventsControl.js
@@ -2,7 +2,8 @@ import { store } from '../../AirElement.js';
 
 export const NEW_EVENT_CREATED = 'NEW_EVENT_CREATED';
 
-const createEvent = payload => {
+const createEvent = event => {
+  const payload = Object.assign({}, event);
   store.dispatch({
     type: NEW_EVENT_CREATED,
     payload
