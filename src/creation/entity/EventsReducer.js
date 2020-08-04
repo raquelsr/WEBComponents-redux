410d3a5f6b48203aaf763fb87f6c2e6776c9db0f
raquelsr <raquelserranor@hotmail.com>
2020-08-04 14:03:08 +0200

    20- Refactor deep copy

--- src/creation/entity/EventsReducer.js
+++ src/creation/entity/EventsReducer.js
@@ -1,14 +1,12 @@
-const deepCopy = input => JSON.parse(JSON.stringify(input));
-
 const NEW_EVENT_CREATED = 'NEW_EVENT_CREATED';
 
 const events = (state = {events: []}, action) => {
   const { type, payload } = action;
   switch (type) {
     case NEW_EVENT_CREATED: 
-      return deepCopy({
+      return {
         events: state.events.concat(payload)
-      });
+      };
   }
   console.log(state, action);
   return state;
--- src/store.js
+++ src/store.js
@@ -1,7 +1,12 @@
 import { createStore } from './lib/redux.js';
 import events from './creation/entity/EventsReducer.js';
+const deepCopy = input => JSON.parse(JSON.stringify(input));
+
+const copyingEvent = (state, action) => {
+  return deepCopy(events(state, action));
+}
 
 const GOOGLE_DEVTOOL = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-const store = createStore(events, GOOGLE_DEVTOOL);
+const store = createStore(copyingEvent, GOOGLE_DEVTOOL);
 
 export default store;
