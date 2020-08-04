04b2430ac80d528f6697643c54fc8088cefa1a65
raquelsr <raquelserranor@hotmail.com>
2020-08-04 12:28:25 +0200

    06 - Add lit-html

--- src/creation/boundary/NewEvent.js
+++ src/creation/boundary/NewEvent.js
@@ -1,6 +1,16 @@
+import { render, html } from '../../lib/lit-html.js';
+
 class NewEvent extends HTMLElement {
   connectedCallback() {
-    this.innerHTML = 'Simple string';
+    const template = html `
+      <input placeholder="name">
+      <button @click=${_ => this.newEvent()}>Create</button>
+    `;
+    render(template, this);
+  }
+
+  newEvent() {
+    console.log('Button clicked!');
   }
 }
 
