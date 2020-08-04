d05e184ff8d755fe1e3f32432dc99f40082fa8c0
raquelsr <raquelserranor@hotmail.com>
2020-08-04 13:58:48 +0200

    18 - Refactor redux reducers

--- src/store.js
+++ src/store.js
@@ -1,21 +1,7 @@
 import { createStore } from './lib/redux.js';
-
-const deepCopy = input => JSON.parse(JSON.stringify(input));
-
-const events = (state = {events: []}, action) => {
-  const { type, payload } = action;
-  switch (type) {
-    case 'NEW_EVENT_CREATED': 
-      return deepCopy({
-        events: state.events.concat(payload)
-      });
-  }
-  console.log(state, action);
-  return state;
-}
+import events from './creation/entity/EventsReducer.js';
 
 const GOOGLE_DEVTOOL = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-
 const store = createStore(events, GOOGLE_DEVTOOL);
 
 export default store;
