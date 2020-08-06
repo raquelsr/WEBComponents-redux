669baa95f033bd74610a7d26aaf1ee3d114385ac
raquelsr <raquelserranor@hotmail.com>
2020-08-06 15:44:02 +0200

    49 - Auto save store and checkbox value

--- src/app.js
+++ src/app.js
@@ -1,2 +1,9 @@
 import './creation/boundary/NewEvent.js';
 import './overview/boundary/EventsOverview.js';
+import { save } from './localstorage/StorageControl.js';
+import store from './store.js';
+
+store.subscribe(() => {
+  const state = store.getState();
+  save(state);
+});
--- src/overview/boundary/EventsOverview.js
+++ src/overview/boundary/EventsOverview.js
@@ -20,7 +20,7 @@ class EventsOverview extends AirElement {
       <tbody>
         ${eventList.filter(e => matchesCriteria(e, filter)).map(({eventname, description, link, checked}) => html `
         <tr> 
-          <td><input name="${eventname}" type="checkbox" checked=${checked} @click=${e => this.triggerSelection(e)}> ${eventname}</td>
+          <td><input name="${eventname}" type="checkbox" ?checked=${checked} @click=${e => this.triggerSelection(e)}> ${eventname}</td>
           <td>${description}</td>
           <td>${link}</td>
       </tr>
